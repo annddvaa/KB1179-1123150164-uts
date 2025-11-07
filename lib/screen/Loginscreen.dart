30ea6ac77ff05bdf4f3e9c867d146c3e879d868f
annddvaa <anandadava93@gmail.com>
2025-11-07 17:12:05 +0700

    menambahkan textfield input email

--- lib/screen/Loginscreen.dart
+++ lib/screen/Loginscreen.dart
@@ -13,6 +13,7 @@ class Loginscreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              const SizedBox(height: 80),
               const Text(
                 'Login',
                 textAlign: TextAlign.center,
@@ -22,10 +23,23 @@ class Loginscreen extends StatelessWidget {
                   color: Colors.black,
                 ),
               ),
+              const SizedBox(height: 20),
               const Text(
-                'Silakan Login Untuk Menlanjutkan',
+                'Silakan Login Untuk Melanjutkan',
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 16, color: Colors.black),
+              ),
+              const SizedBox(height: 30),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'Masukkan email anda',
+                  prefixIcon: const Icon(Icons.email_outlined),
+                  border: OutlineInputBorder(
+                    borderRadius: const BorderRadius.all(Radius.circular(12)),
+                  ),
+                ),
               ),
             ],
           ),
